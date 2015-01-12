8a8d409370a4bb28ac9a8cdc898e7649d16fd27a
Ahmadul <Ahmadul@Nabeela-2>
2015-01-11 21:57:41 -0600

    v0.6.2 * added a little bit to the help menu

--- CodeVersusBugsProject7/src/clientfiles/Help.java
+++ CodeVersusBugsProject7/src/clientfiles/Help.java
@@ -1,9 +1,11 @@
 package clientfiles;
 
+import java.awt.Color;
+import java.awt.Font;
+
 import javafx.scene.control.ScrollBar;
 
-import javax.swing.JFrame;
-import javax.swing.JTextField;
+import javax.swing.*;
 
 /**
  * Creates a help menu in order to avoid the need for a tutorial
@@ -14,19 +16,36 @@ import javax.swing.JTextField;
 
 class Help extends JFrame
 {
-	public static JTextField helpMenu;
-	public static ScrollBar scroll;
+	public JTextArea helpMenu;
+	public JScrollPane scroll;
 	
 	public Help()
 	{
-		// 
-		helpMenu = new JTextField("HELP:\n\n\n"
+		// TODO add scroll bar
+		helpMenu = new JTextArea("HELP:\n\n\n"
 								+ "Malwares:\n\n"
 								+ "Minions:\n"
-								+ "Minions are low level malware taht persist throughout the game, there are 3 versions: regular (weakest), small (fast), large (slow and more health)\n\n"
+								+ "Minions are low level malware that persist throughout the game. There are 3 versions: regular/blue (weakest), small/green (fast), large/red (slow and more health)\n\n"
 								+ "Viruses:\n"
-								+ "Viruses are a later game malware that replicate when they hit a packet or file, there are 3 versions like above\n\n"
+								+ "Viruses are a later game malware that replicate when they hit a packet or file. There are 3 versions of viruses, like above.\n\n"
 								+ "Trojans:\n"
-								+ "Trojans come from the bottom file path and are invisible to anti-malware devices until");
+								+ "Trojans come from the bottom file path and are invisible to anti-malware devices until they are either scanned or they run into an upgraded encryptor\n\n"
+								+ "Spywares:\n"
+								+ "Spywares are late game and attempt to steal money (10%) by retrievng a file and returning back to the start. If they don't find a file, they'll be happy to damage the CPU instead.\n\n"
+								+ "Worms:\n"
+								+ "Worms are late game malware that have a lot of health aand can fire back, causing infections. Infected devices are far less effective. Its orange \"tail\" indicates its health.\n\n"
+								+ "Bots:\n"
+								+ "Bots are end game malwares that have lots of health, move quickly and spawn malwares whenever hit by projectiles\n\n\n\n"
+								+ "Anti-Malware Devices:\n\n"
+								+ "DiscThrower:\n");
+		helpMenu.setEditable(false);
+		helpMenu.setWrapStyleWord(true);
+		helpMenu.setForeground(new Color(0, 162, 232));
+		helpMenu.setFont(new Font("Monospaced", Font.PLAIN, 15));
+		helpMenu.setCaretPosition(0);
+		
+		//scrollbar stuff
+		scroll = new JScrollPane(helpMenu);
+		scroll.setVerticalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 	}
 }
